727cf087eb4cdc54e7e0d76932c11f24ed372489
veeresh97 <67183089+veeresh97@users.noreply.github.com>
2021-05-25 22:41:08 +0530

    Added: welcome message

---
+++ main/java/com/EmpPayRoll/EmployeePayrollService.java
@@ -0,0 +1,7 @@
+package com.EmpPayRoll;
+
+public class EmployeePayrollService {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee payroll service program");
+    }
+}
