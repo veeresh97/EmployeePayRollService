7434c0d27ad95dbed4dced539753b0bca11d6c97
veeresh97 <67183089+veeresh97@users.noreply.github.com>
2021-05-26 23:20:58 +0530

    Added: watch service

--- test/java/com/EmpPayRoll/NIOFileAPITest.java
+++ test/java/com/EmpPayRoll/NIOFileAPITest.java
@@ -43,4 +43,10 @@ public class NIOFileAPITest {
         Files.newDirectoryStream(playPath , path -> path.toFile().isFile() && path.toString().startsWith("temp"))
                 .forEach(System.out::println);
     }
+    @Test
+    public void givenADirectoryWhenWatchedListsAllTheActivities() throws IOException {
+        Path dir = Paths.get(HOME + "/" + PLAY_WITH_NIO);
+        Files.list(dir).filter(Files::isRegularFile).forEach(System.out::println);
+        new Java8WatchServiceExample(dir).processEvents();
+    }
 }
